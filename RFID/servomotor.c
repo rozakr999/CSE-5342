be7bc2de542ad0d1d35dc9eedaf1a33d326f7ae4
J-Cuin <123055499+J-Cuin@users.noreply.github.com>
2025-12-01 08:45:59 -0600

    Update servomotor.c

--- RFID/servomotor.c
+++ RFID/servomotor.c
@@ -1,6 +1,6 @@
 //Final project for Embedded2
 //PWM setup
-//PA9 as PWM output using TIM1_CH1
+//PA9 as PWM output using TIM1_CH2
 
 //using STM32F446RE Nucleo board
 //using SG90 servo motor
@@ -23,17 +23,17 @@ void delay_Ms(int n)
 	}
 }
 
-//pwm config using tim1_ch1 
+//pwm config using tim1_ch2
 //pwm frequency 50 Hz
 void initPwm(void)
 {
-	//enable clocks for GPIOA and GPIOB and TIM1
+	//enable clocks for GPIOA and TIM1
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
 	(void)RCC->AHB1ENR; //dummy read
 	
-	//PA8 as alternate function
-	PWM_GPIO_PORT->MODER &= ~(3U << (2*PWM_GPIO_PIN));
+	//PA9 as alternate function
+	PWM_GPIO_PORT->MODER &= ~(3U << (2*PWM_GPIO_PIN)); //clear mode bits
 	PWM_GPIO_PORT->MODER |= (2U << (2*PWM_GPIO_PIN)); //AF mode
 	PWM_GPIO_PORT->AFR[PWM_GPIO_PIN >> 3] &= ~(0xFU << (4*(PWM_GPIO_PIN & 7)));
 	PWM_GPIO_PORT->AFR[PWM_GPIO_PIN >> 3] |= (1U << (4*(PWM_GPIO_PIN & 7))); //AF1 FOR TIM1?
@@ -44,23 +44,23 @@ void initPwm(void)
 	TIM1->ARR = 20000 - 1; //2000 -> 20,000 us = 20ms (50 Hz)
 	TIM1->CCR1 = 1500; //default pulse width 1.5ms (neutral posititon)
 	
-	//configure channel 1: pwm mode 1, preload enable
+	//configure channel 2: pwm mode 1, preload enable
 	TIM1->CCMR1 &= ~(TIM_CCMR1_OC2M);
 	TIM1->CCMR1 |= (6U << TIM_CCMR1_OC2M_Pos); // PWM MODE 1
 	TIM1->CCMR1 |= TIM_CCMR1_OC2PE; //PRELOAD ENABLE
 	
-	TIM1->CCER |= TIM_CCER_CC2E; // ENABLE CHANNEL 2 OUTPUT
+	TIM1->CCER |= TIM_CCER_CC2E; // ENABLE CHANNEL OUTPUT
 	TIM1->BDTR |= TIM_BDTR_MOE; // ENABLE MAIN OUTPUT
 	TIM1->CR1 |= TIM_CR1_ARPE; // ENEABLE ARPE
-	TIM1->EGR |= TIM_EGR_UG; // GENERATE UPDATE EVENT  OT LOAD THE PRESCALER VALUE
+	TIM1->EGR |= TIM_EGR_UG; // GENERATE UPDATE EVENT
 	TIM1->CR1 |= TIM_CR1_CEN; // START TIMER COUNTER
 	
 }
 
-//set sevo pulde width directly in microseconds
+//set servo pulse width directly in microseconds
 void setServoPulse(uint16_t pulseWidth_us)
 {
-	//keep pu;se width in safe range
+	//keep pulse width in safe range
 	if (pulseWidth_us < 500) pulseWidth_us = 500;
 	if (pulseWidth_us > 2500) pulseWidth_us = 2500;
 	TIM1->CCR2 = pulseWidth_us; //write pulse width to timers compar register
@@ -78,36 +78,19 @@ void setServoAngle(uint8_t angle)
 
 void sweepServo(void)
 {
-	//sweep servo 0 -> 180
-		for (uint8_t angle = 0; angle <= 180; angle += 5) // angle per step; lower number for slower sweep
+	//sweep servo 75 -> 0
+		for (uint8_t angle = 75; angle > 0; angle -= 5) // angle per step; lower number for slower sweep
 		{
 			setServoAngle(angle);
 			delay_Ms(50); //delay per step; lower number for smaller delay = faster
 		}
 		delay_Ms(1000); //pause for 1 seconds
 		
-		//sweep back 180 -> 0
-		for (uint8_t angle = 180; angle > 0; angle -=5)
+		//sweep back 0 -> 75
+		for (uint8_t angle = 0; angle <= 75; angle +=5)
 		{
 			setServoAngle(angle);
 			delay_Ms(50);
 		}
 		delay_Ms(1000); 
 	}
-	
-/*
-int main(void)
-{
-	SystemInit();
-	SystemCoreClockUpdate();
-	//uint32_t sysclk = SystemCoreClock;
-	initPwm();
-	
-	while (1)
-	{
-		sweepServo();
-	}
-}
-*/
-
-
